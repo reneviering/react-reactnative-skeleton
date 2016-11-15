75ed9a26b336ccf5886d3988da5c3ab40e600770
René Viering <r.viering@micromata.de>
2016-11-15 21:46:57 +0100

    Refactor

--- react/src/app/index.js
+++ react/src/app/index.js
@@ -5,9 +5,11 @@ import AboutRoute from './about';
 import rootReducer from './reducers/rootReducer';
 import {createActionCreator} from './actions/createActionCreator';
 
+// Init redux
 const store = createStore(rootReducer);
 const actionCreator = createActionCreator(store);
 
+// Init router
 const router = createRouter('app');
 
 router.addRoute('', {
@@ -25,12 +27,12 @@ router.addRoute('/', {
 router.addRoute('home', {
 	init: HomeRoute.init,
 	dispose: HomeRoute.dispose,
-	data: {store: store, actionCreator: actionCreator}
+	data: {store: store, actionCreator: actionCreator} // pass store and actionCreator to the route handler
 });
 router.addRoute('about', {
 	init: AboutRoute.init,
 	dispose: AboutRoute.dispose,
-	data: {store: store, actionCreator: actionCreator}
+	data: {store: store, actionCreator: actionCreator} // pass store and actionCreator to the route handler
 });
 
 router.addRoute('*', {
